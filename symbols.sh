e964ddbfc53c345731ae656585df2b9800bd3de9
Eduardo Folly <edufolly@gmail.com>
2025-02-02 11:43:38 -0300

    Updating Gradle and Android plugins.

--- symbols.sh
+++ symbols.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd build/app/intermediates/stripped_native_libs/release/out/lib/ || exit 10
+cd build/app/intermediates/stripped_native_libs/release/stripReleaseDebugSymbols/out/lib/ || exit 10
 
 rm .DS_Store
 
@@ -8,4 +8,6 @@ zip -r symbols.zip .
 
 mv -f symbols.zip ~/Desktop/
 
-cd ../../../../../../../
+cd ../../../../../../../../
+
+cp build/app/outputs/bundle/release/app-release.aab ~/Desktop/ || exit 5
